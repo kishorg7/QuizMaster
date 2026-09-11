2386f4f1e85b06b26c849be752a537b20e6d04f1
Kishor <kishukishorpersonal@gmail.com>
2026-09-11 13:09:47 +0530

    Add quiz questions data

---
+++ questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "High Text Machine Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language"
+        ],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "Which language is used to style a web page?",
+        options: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        answer: "CSS"
+    },
+    {
+        question: "Which language is used to make web pages interactive?",
+        options: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "SQL"
+        ],
+        answer: "JavaScript"
+    },
+    {
+        question: "Which HTML element is used to create a hyperlink?",
+        options: [
+            "<link>",
+            "<a>",
+            "<href>",
+            "<url>"
+        ],
+        answer: "<a>"
+    },
+    {
+        question: "Which symbol is used for an ID selector in CSS?",
+        options: [
+            ".",
+            "#",
+            "*",
+            "@"
+        ],
+        answer: "#"
+    }
+];
